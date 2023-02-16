e1b36c58dfe861c0e34d3347d356b053b956f7ea
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-02-16 20:08:33 +0530

    Completed 16x16 setup of grid and basic UI

---
+++ javascript.js
@@ -0,0 +1,21 @@
+let hoveringArea = document.getElementById("sketchPad")
+
+function makeGrid(row,col){
+    hoveringArea.style.setProperty("--grid-row" , row);
+    hoveringArea.style.setProperty("--grid-col" , col);
+
+    for(c = 0; c < (row * col); c++){
+        let cell = document.createElement("div");
+        hoveringArea.appendChild(cell).id = "gridCell";
+    }
+    let cell = document.getElementById("gridCell")
+    cell.addEventListener("mouseover",hoverTrail)
+
+    function hoverTrail(){
+        cell.style.background = 'blue';
+    }
+}
+
+window.onload = function() {
+    makeGrid(16,16);
+}
