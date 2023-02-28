4d6eaf04ce16a27d4018d9be7a74c8e909df2418
10234567Z <harshsuthar703@gmail.com>
2023-03-01 03:05:42 +0530

    Added resizing of grid

--- javascript.js
+++ javascript.js
@@ -4,12 +4,34 @@ function makeGrid(row, col) {
     hoveringArea.style.setProperty("--grid-row", row);
     hoveringArea.style.setProperty("--grid-col", col);
 
+    /** Until cell is equal to area , increment it */
     for (c = 0; c < (row * col); c++) {
         let cell = document.createElement("div");
         hoveringArea.appendChild(cell).id = "gridCell";
+
+        /** Upon hover change background by getting active ID with purple bgColor */
         cell.addEventListener('mouseover',function changeBackground(){
-            this.classList.add('active');
+            this.setAttribute('id','active')
         })
     }
 }
-makeGrid(16, 16);
+function gridSize(){
+    makeGrid(16,16);
+    let slider = document.getElementById('gridSize');
+    let sliderValue = document.getElementById('value')
+    sliderValue.innerHTML = slider.value + 'x' + slider.value;
+    slider.oninput = function(){
+        sliderValue.innerHTML = this.value + 'x' + this.value;
+        let size = slider.value;
+        resetGrid();
+        makeGrid(size,size);
+    }
+}
+function resetGrid(){
+    hoveringArea.remove();
+    hoveringArea = document.createElement('div');
+    hoveringArea.id = 'sketchPad';
+    document.getElementById('sketchArea').appendChild(hoveringArea);
+}
+
+gridSize();
